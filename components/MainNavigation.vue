bde4ad161801e5f7836c47b46bd58e2b955e74c9
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2022-09-23 12:02:20 +0200

    Remove left-over debug.

--- components/MainNavigation.vue
+++ components/MainNavigation.vue
@@ -31,7 +31,6 @@ export default {
   mounted () {
     // Fetch menu in SPA mode. Should be filled already in SSR mode, so this is a fallback.
     if (!this.menus.main?.length) {
-      console.log('fetch')
       this.$drupal.fetchMenu('main')
     }
   }
