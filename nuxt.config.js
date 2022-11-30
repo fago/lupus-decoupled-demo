d7eaf5c98346644efc8fa12d8c1f0a7d165441d0
Wolfgang Ziegler <fago@users.noreply.github.com>
2022-11-30 16:08:49 +0100

    Force markup request format since the backend defaults to JSON now (#2)
    * Update dependencies.
    
    * Force content format to markup for now.
    
    * Update nuxtjs-drupal-ce to v1.1.1.
    
    * Remove debugging.
    
    * Remove debug.
    
    Co-authored-by: Matic Fink <turtlman@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,10 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  ssr: false,
+  ssr: true,
+  axios: {
+    debug: false
+  },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -29,8 +32,9 @@ export default {
 
   'drupal-ce': {
     baseURL:
-      'http://example.d9playground.localdev.space/ce-api',
+      'https://8080-shaal-drupalpod-gs7ruhbg9u9.ws-eu77.gitpod.io/ce-api',
     addRequestFormat: false,
+    addRequestContentFormat: 'markup',
     useProxy: false,
   },
 
